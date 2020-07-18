e2c0182f6ce109c143acc4370fbb052363f6eec3
Diego <diegobustos1229@gmail.com>
2020-07-17 18:27:57 -0700

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .views import BlogListView, BlogDetailView
+from django.urls import path
+
+urlpatterns = [
+    path('post/<int:pk>', BlogDetailView.as_view(), name='post_detail'),
+    path('', BlogListView.as_view(), name='home'),
+]
